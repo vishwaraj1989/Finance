23f21ab3debfcd0dca192a3a184db67fdcd85338
NearMeWebsite <nearmewebsites@gmail.com>
2025-06-20 16:02:54 +0530

    Switched all routes from bcrypt to bcryptjs

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.use("/api/consumer-name", getConsumerNameRoute);
 app.use("/api/consumer-details", getConsumerDetailsRoute); // <-- here is your diaryNumber lookup
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
